0a3866c982c7509ae419854e75a7c25ee6258ca5
Trung Luu <luuvantrung@gmail.com>
2016-10-04 15:51:18 +0700

    Update main.py

--- Structural/DepencyInjection/main.py
+++ Structural/DepencyInjection/main.py
@@ -4,7 +4,7 @@ from classes.ArrayConfig import ArrayConfig
 # Dependency Injection
 # Purpose : To implement a loosely coupled architecture in order to get better stable, maintainable and extendable code
 # ArrayConfig is injected into Connection.
-# A Connection (client) just use two functions get and set of config object base on an interface Parameters
+# A Connection (client) just use two functions get and set of config object is defined from an interface Parameters
 # and don't care about how config object is implemented
 
 
